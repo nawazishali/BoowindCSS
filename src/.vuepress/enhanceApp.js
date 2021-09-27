da4a634d7caf301cd3ed54e4e926359e2e1b8f8d
Nawazish Ali <nawazishaliejaz@gmail.com>
2021-09-27 22:48:10 +0500

    Enhancements: - Enhance the functionality of code-preview component. - code-preview component can now display template code automitcally using the slot data. - Add a custom code highligher using highlight.js. - Add code formatting using prettier js. - Add copy to clipboard functionality using a highlight.js plugin and fontawesome. - update breadcrumbs to see the changes in action.

--- src/.vuepress/enhanceApp.js
+++ src/.vuepress/enhanceApp.js
@@ -4,6 +4,10 @@
  * https://v1.vuepress.vuejs.org/guide/basic-config.html#app-level-enhancements
  */
 
+//  import hljs from 'highlight.js/lib/common';
+//  import hljsVuePlugin from "@highlightjs/vue-plugin";
+import CodeEditor from 'simple-code-editor';
+
 export default ({
   Vue, // the version of Vue being used in the VuePress app
   options, // the options for the root Vue instance
@@ -11,4 +15,5 @@ export default ({
   siteData // site metadata
 }) => {
   // ...apply enhancements for the site.
+  Vue.component('code-editor', CodeEditor)
 }
