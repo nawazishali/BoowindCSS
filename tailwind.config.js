fbd5a73f84fd69c75f492fc8dd6561a86f377e01
Nawazish <n.ali@metru.io>
2021-10-03 06:12:17 +0500

    - Configure TailwindCSS pseudo class variants. - Upgrade Tailwind version from 1.8.10 to 1.9.6 - Add Buttons template page. - Enable the !important css tag for all Tailwind classes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    backgroundColor: ["active", "focus", "hover", "disabled"],
+    textColor: ["active", "focus", "hover", "disabled"],
+    textDecoration: ["active", "focus", "hover", "disabled"],
+    boxShadow: ["active", "focus", "hover", "disabled"],
+    opacity: ["active", "focus", "hover", "disabled"]
+  },
   plugins: [],
-}
+  important: true, // https://sebastiandedeyne.com/why-we-use-important-with-tailwind/
+};
